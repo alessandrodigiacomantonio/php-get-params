6498992f149f603977963e2185b4418bee4eab01
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-02-21 16:31:01 +0100

    cambiato (int) in is_numeric()

--- snack2/index.php
+++ snack2/index.php
@@ -1,8 +1,8 @@
 <?php
   $name = $_GET['name'];
   $mail = $_GET['email'];
-  $age = (int)$_GET['age'];
-  if ( strlen($name) > 3 && strpos($mail, '@') !== false && strpos($mail, '.') !== false && is_int($age) ) $access = 'riuscito';
+  $age = $_GET['age'];
+  if ( strlen($name) > 3 && strpos($mail, '@') !== false && strpos($mail, '.') !== false && is_numeric($age) ) $access = 'riuscito';
   else $access = 'negato';
 ?>
 
